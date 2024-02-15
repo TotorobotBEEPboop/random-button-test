f3269dbaf63af05df78506abb2f8a96518765401
Totorobot <thom.fairbrother22@gmail.com>
2024-02-15 22:26:53 +0900

    Yaaaaas

---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const Button = () => {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div className="layer-three">
+      <div className="layer-two">
+        <div className="layer-one">
+          <button
+            onClick={() => setCounter(counter + 1)}
+            type="button"
+            className="btn btn-primary"
+          >
+            <span className="but-text">Hi!</span>
+          </button>
+          <p className="text">{counter}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Button;
