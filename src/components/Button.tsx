24533efd625ceff661842cd74e972e9fbe558192
dbjowett <dbjowett@gmail.com>
2024-02-16 06:52:19 +0900

    Add cool new feature

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,15 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { getRandomBackground } from '../utils';
 
 const Button = () => {
   const [counter, setCounter] = useState(0);
+  const randomBackground = getRandomBackground(1000);
 
   return (
-    <div className="layer-three">
+    <div style={{ background: randomBackground }} className={'layer-three'}>
       <div className="layer-two">
         <div className="layer-one">
-          <button
-            onClick={() => setCounter(counter + 1)}
-            type="button"
-            className="btn btn-primary"
-          >
+          <button onClick={() => setCounter(counter + 1)} type="button" className="btn btn-primary">
             <span className="but-text">Hi!</span>
           </button>
           <p className="text">{counter}</p>
---
+++ src/utils.ts
@@ -0,0 +1,8 @@
+export const getRandomBackground = (numberOfColors: number): string => {
+  const generateRandomHSLColor = () => `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
+  const randomColors = Array.from({ length: numberOfColors }, generateRandomHSLColor);
+  const shuffleArray = (array: string[]) => array.slice().sort(() => Math.random() - 0.5);
+  const shuffledColors = shuffleArray(randomColors);
+
+  return `conic-gradient(${shuffledColors.join(', ')})`;
+};
