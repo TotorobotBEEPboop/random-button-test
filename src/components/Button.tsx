e904495e7f610005863bb6cec1f03518791e0cdc
Totorobot <thom.fairbrother22@gmail.com>
2024-02-19 11:35:28 +0900

    Hey wow woah what's happening

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,22 @@
-import { useState } from 'react';
-import { getRandomBackground } from '../utils';
+import { useState } from "react";
+import { getRandomBackground } from "../utils";
 
-const NUMBER_OF_COLOURS = 1000;
+const NUMBER_OF_COLOURS = 10;
 
 const Button = () => {
   const [counter, setCounter] = useState(0);
   const randomBackground = getRandomBackground(NUMBER_OF_COLOURS);
+  const increaseCounter = () => setCounter(counter + 1);
 
   return (
-    <div style={{ background: randomBackground }} className={'layer-three'}>
+    <div style={{ background: randomBackground }} className={"layer-three"}>
       <div className="layer-two">
         <div className="layer-one">
-          <button onClick={() => setCounter(counter + 1)} type="button" className="btn btn-primary">
+          <button
+            onClick={increaseCounter}
+            type="button"
+            className="btn btn-primary"
+          >
             <span className="but-text">Hi!</span>
           </button>
           <p className="text">{counter}</p>
